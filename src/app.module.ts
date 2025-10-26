d9e019de97078eedc72508fe2070a5a67909798d
unknown <rizkyalphazee@gmail.com>
2025-10-26 21:36:32 +0700

    feat: setup typeorm and postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PassportModule } from '@nestjs/passport';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { DatabaseController } from './database.controller';
 import { DatabaseService } from './database.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DATABASE_HOST'),
+        port: config.get('DATABASE_PORT'),
+        username: config.get('DATABASE_USERNAME'),
+        password: config.get('DATABASE_PASSWORD'),
+        database: config.get('DATABASE_NAME'),
+        synchronize: true,
+      })
+    }),
+  ],
   controllers: [DatabaseController],
   providers: [DatabaseService],
   exports: [DatabaseService]
